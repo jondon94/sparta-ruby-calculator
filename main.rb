d33a0dabc910fb611b746e2c086ba3a5285e3bab
Tech-A66 <tech-a66@Tech-A66s-Air.home>
2017-11-14 23:07:55 +0000

    advanced and basic calculator are working, BMI is not yet but i know the reason. Trip has not started yet

---
+++ main.rb
@@ -0,0 +1,69 @@
+# Ruby Calculator
+
+puts "Welcome to Sparta-Ruby_Calculator, Please Enter Your Name:"
+name = gets.chomp
+puts "Hello " + name + ", Which calculator would you like to use? (a)dvanced, (b)asic or (c)BMI?"
+type = gets.chomp
+
+# binding.pry
+if type == "a"
+  #ADVANCED CALCULATOR
+  puts 'You have chosen the advanced calculator. Here you can use exponents or root ooperators'
+  puts "Choose which operator you would like to use an e(x)ponent or a (r)oot?"
+  opB = gets.chomp
+  if opB == "r"
+    puts "Choose the number you wish to square root"
+    n1 = gets.chomp.to_i
+    answer = Math.sqrt(n1)
+    puts answer
+  elsif opB == "x"
+    puts "Choose your first number"
+    n1 = gets.chomp.to_i
+    puts "Now choose your second number:"
+    n2 = gets.chomp.to_i
+    answer = n1**n2
+    puts answer
+  end
+
+elsif type == "b"
+  #BASIC CALCULATOR
+  puts 'You have chosen the basic calculator. Here you can add, subtract, multiply and divide. Firstly, input your first number:'
+  n1 = gets.chomp.to_i
+  puts "Now choose your second number:"
+  n2 = gets.chomp.to_i
+  puts "Choose which operator you would like to use a(d)d, (s)ubtract, (m)ultiply or di(v)ide?"
+  opB = gets.chomp
+    if opB == "d"
+      answer = n1 + n2
+      puts answer
+    elsif opB == "s"
+      answer = n1 - n2
+      puts answer
+    elsif opB == "m"
+      answer = n1 * n2
+      puts answer
+    elsif opB == "v"
+      answer = n1 / n2
+      puts answer
+    else
+      puts "Im sorry, lets start again"
+    end
+
+elsif type == "c"
+  #BMI CALCULATOR
+  puts 'Here you can calculate your BMI'
+  puts 'Indicate which system you would like to use, (m)etric or (i)mperial'
+  sys = gets.chomp
+  if sys == "m"
+    puts "You chosen to use the metric system, a wise choice. Please input you weight in kilograms"
+    weight = gets.chomp.to_i
+    puts "Please input your Height in metres"
+    height = gets.chomp.to_i
+    bmi = (weight / ( height * height ))
+    puts bmi
+  elsif sys == "i"
+    puts "You have chosen to use the imperial system"
+  end
+else
+  puts "I'm sorry " + name + " please try again."
+end
